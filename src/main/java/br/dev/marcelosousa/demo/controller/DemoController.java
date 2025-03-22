a6a1c471c943ffad4d2e8ce4b41aec1e859dfc0b
Marcelo Sousa <mns.marcelo67@gmail.com>
2025-03-22 17:41:16 -0300

    add logs

--- src/main/java/br/dev/marcelosousa/demo/controller/DemoController.java
+++ src/main/java/br/dev/marcelosousa/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package br.dev.marcelosousa.demo.controller;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class DemoController {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/healthcheck")
     public ResponseEntity<String> healthcheck() {
+        log.atInfo().log("health check");
         return ResponseEntity.ok("OK");
     }
 
     @GetMapping("/")
     public ResponseEntity<String> home() {
+        log.atInfo().log("Home");
         return ResponseEntity.ok("OK, home");
     }
 }
