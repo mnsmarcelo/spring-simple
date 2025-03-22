fbb7b3b3c7e627ae37949a95878eb081a46087b9
Marcelo Sousa <mns.marcelo67@gmail.com>
2025-03-21 21:32:39 -0300

    iniciando projeto

---
+++ src/main/java/br/dev/marcelosousa/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package br.dev.marcelosousa.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthcheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
